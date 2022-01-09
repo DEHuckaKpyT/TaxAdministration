c6f49f5e3dfd599a20968f0c8daad8a773087a5d
Матыцин Денис Олегович <den-matytsin@mail.ru>
2022-01-09 19:08:18 +1000

    /

--- TaxAdministration/form/FormSearchOrganization.cs
+++ TaxAdministration/form/FormSearchOrganization.cs
@@ -19,6 +19,7 @@ namespace TaxAdministration.form
 
         private void button1_Click(object sender, EventArgs e)
         {
+
             if (textBox1.Text != "")
             {
                 Organization organization = Repository.Execute<Organization>("SearchOrganization",
